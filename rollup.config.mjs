4394bb4972d922b62c275894cb2269b3a269eccb
aswinshenoy <aswinshenoy65@gmail.com>
2024-01-27 09:22:35 +0530

    Release [1.0.0-beta.2] - Fix node:crypto error

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,25 +2,21 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
 import terser from '@rollup/plugin-terser';
-import json from '@rollup/plugin-json';
+import preserveDirectives from "rollup-plugin-preserve-directives";
 
 import dts from 'rollup-plugin-dts';
 import postcss from 'rollup-plugin-postcss';
 import nodePolyfills from 'rollup-plugin-polyfill-node';
-
-import packageJson from './package.json' assert { type: 'json' };
 
 export default [
   {
     input: 'src/index.ts',
     output: [
       {
-        file: packageJson.main,
-        format: 'cjs',
-      },
-      {
-        file: packageJson.module,
-        format: 'esm',
+        dir: 'dist',
+        preserveModules: true,
+        exports: 'named',
+        entryFileNames: '[name].js',
       },
     ],
     plugins: [
@@ -33,17 +29,18 @@ export default [
       postcss({
         modules: true,
         minimize: true,
-
       }),
       terser(),
       nodePolyfills(),
-      json(),
+      preserveDirectives({
+        supressPreserveModulesWarning: true,
+      }),
     ],
-    external: ["react", "react-dom"]
+    external: ["react", "react-dom", "nanoid"]
   },
   {
-    input: 'dist/esm/types/index.d.ts',
-    output: [{ file: 'dist/index.d.ts', format: 'esm' }],
+    input: 'dist/types/index.d.ts',
+    output: [{ dir: 'dist', format: 'esm' }],
     plugins: [dts()],
   },
 ];
